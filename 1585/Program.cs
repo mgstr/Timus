16e36dccade4d5c5c2778727cda1b70745703917
Andres Puulinn <andres.puulinn@gmail.com>
2011-12-27 18:27:03 +0200

    Solution for "1585. Penguins"

---
+++ 1585/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _1585
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var count = int.Parse(Console.ReadLine());
+            var map = new Dictionary<string, int>();
+
+            while (count-- != 0)
+            {
+                var name = Console.ReadLine();
+                if (map.ContainsKey(name))
+                    ++map[name];
+                else
+                    map.Add(name, 1);
+            }
+
+            var max = map.Values.Max();
+            Console.WriteLine(map.First(p => p.Value == max).Key);
+        }
+    }
+}
