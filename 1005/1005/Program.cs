637be06cdfcc9a70c601472469c76565bc06aa0d
Andres Puulinn <andres.puulinn@gmail.com>
2011-12-28 22:29:21 +0200

    Brute force solution, but if was accepted

--- 1005/1005/Program.cs
+++ 1005/1005/Program.cs
@@ -8,15 +8,27 @@
         {
             int stoneCount = int.Parse(Console.ReadLine());
 
-            var stones = new List<int>();
+            var temp = new List<int>();
             foreach (var stoneWeight in Console.ReadLine().Split(" ".ToCharArray()))
-                stones.Add(int.Parse(stoneWeight));
+                temp.Add(int.Parse(stoneWeight));
+            var stones = temp.ToArray();
 
-            int totalWeight = 0;
-            foreach (var stoneWeight in stones)
-                totalWeight += stoneWeight;
+            long minDifference = long.MaxValue;
+            for (long mask = (1L << stoneCount) - 1; mask != 0; --mask)
+            {
+                long total = 0;
+                for (long flag = 1, stone = 0; stone < stoneCount; ++stone, flag <<= 1)
+                    if ((flag & mask) != 0)
+                        total += stones[stone];
+                    else
+                        total -= stones[stone];
+                long difference = Math.Abs(total);
 
-            Console.WriteLine(totalWeight);
+                if (minDifference > difference)
+                    minDifference = difference;
+            }
+
+            Console.WriteLine(minDifference);
         }
     }
 }
