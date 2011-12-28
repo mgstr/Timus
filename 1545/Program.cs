4fcecea6dc2e19fcd1a2ffda29fe750822b5174e
Andres Puulinn <andres.puulinn@gmail.com>
2011-12-28 23:06:10 +0200

    Solution for "1545. Hieroglyphs"

---
+++ 1545/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1545
+{
+    /// <summary>
+    /// http://acm.timus.ru/problem.aspx?space=1&num=1545
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            var words = int.Parse(Console.ReadLine());
+
+            var map = new Dictionary<char, string>();
+
+            for (int word = 1; word <= words; ++word)
+            {
+                var text = Console.ReadLine();
+                char letter = text[0];
+                if (map.ContainsKey(letter))
+                    map[letter] += "\r\n" + text;
+                else
+                    map.Add(letter, text);
+            }
+
+            var first = Console.ReadLine()[0];
+            if (map.ContainsKey(first))
+                Console.WriteLine(map[first]);
+        }
+    }
+}
