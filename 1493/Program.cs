5462fe7948a899c5f174d369f957a98c76dd86cb
Andres Puulinn <andres.puulinn@gmail.com>
2011-12-28 23:38:37 +0200

    Solution for "1493. One Step from Happiness"

---
+++ 1493/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1493
+{
+    /// <summary>
+    /// http://acm.timus.ru/problem.aspx?space=1&num=1493
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Lucky(number - 1) || Lucky(number + 1) ? "Yes" : "No");
+        }
+
+        private static bool Lucky(int number)
+        {
+            return Digit(number, 0) + Digit(number, 1) + Digit(number, 2)
+                == Digit(number, 3) + Digit(number, 4) + Digit(number, 5);
+        }
+
+        private static int Digit(int number, int digit)
+        {
+            return (number / Convert.ToInt32(Math.Pow(10, digit))) % 10;
+        }
+    }
+}
