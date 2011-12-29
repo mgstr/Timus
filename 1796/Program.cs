06b910b7e464a1f2d45c786269a4d9a9ecd72920
Andres Puulinn <andres.puulinn@gmail.com>
2011-12-29 23:06:08 +0200

    Solution for "1796. Amusement Park"

---
+++ 1796/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1796
+{
+    class Program
+    {
+        static void Main()
+        {
+            var nominals = new[] { 10, 50, 100, 500, 1000, 5000 };
+
+            var money = Console.ReadLine().Split(" ".ToCharArray()).Select(t => int.Parse(t)).ToArray();
+            var price = int.Parse(Console.ReadLine());
+
+            var total = 0;
+            var min = int.MaxValue;
+            for (var i = 0; i < nominals.Length; ++i)
+            {
+                total += money[i] * nominals[i];
+                if (money[i] != 0 && min == int.MaxValue)
+                    min = nominals[i];
+            }
+
+            var maxCount = total / price;
+            var minCount = 1 + (total - min) / price;
+            var count = maxCount - minCount + 1;
+            Console.WriteLine(count);
+            for (int c = 0; c < count; ++c)
+            {
+                Console.Write(minCount + c);
+                Console.Write(" ");
+            }
+        }
+    }
+}
